fd6c681675af9069f5ee539b3fee9dcd3c79da86
Geoff Whitehead <geoff1012@gmail.com>
2020-02-10 14:39:58 +0000

    add script to rebuild modules

--- packages/pos-server/app/app.js
+++ packages/pos-server/app/app.js
@@ -1,15 +1,15 @@
 // npm packages
-const dotenv = require('dotenv');
-const express = require('express');
-Promise = require('bluebird'); // eslint-disable-line
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
+const dotenv = require("dotenv");
+const express = require("express");
+Promise = require("bluebird"); // eslint-disable-line
+const bodyParser = require("body-parser");
+// const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
 // app imports
-const { connectToDatabase, globalResponseHeaders } = require('./config');
-const { errorHandler } = require('./handlers');
-const { usersRouter } = require('./routers');
+const { connectToDatabase, globalResponseHeaders } = require("./config");
+const { errorHandler } = require("./handlers");
+const { usersRouter } = require("./routers");
 
 // global constants
 dotenv.config();
@@ -35,19 +35,18 @@ app.use(bodyParser.json());
 
 // response headers setup; CORS
 // enabling CORS for all requests
-app.use(cors());
-// app.use(globalResponseHeaders);
+// app.use(cors());
+app.use(globalResponseHeaders);
 
 // adding morgan to log HTTP requests
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
-
-app.use('/users', usersRouter);
+app.use("/users", usersRouter);
 
 // catch-all for 404 "Not Found" errors
-app.get('*', fourOhFourHandler);
+app.get("*", fourOhFourHandler);
 // catch-all for 405 "Method Not Allowed" errors
-app.all('*', fourOhFiveHandler);
+app.all("*", fourOhFiveHandler);
 
 app.use(globalErrorHandler);
 
