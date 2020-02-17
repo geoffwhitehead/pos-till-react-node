6b0036ef9c1aef1df3cbac6196f284139539b2fb
Geoff Whitehead <geoff1012@gmail.com>
2020-02-17 21:05:27 +0000

    add models to index, rename Modifiers to Modifier

--- packages/pos-server/app/models/index.ts
+++ packages/pos-server/app/models/index.ts
@@ -1,1 +1,5 @@
 export { User } from './User';
+export { Item } from './Item';
+export { Category } from './Category';
+export { Discount } from './DIscount';
+export { Modifier } from './Modifier';
