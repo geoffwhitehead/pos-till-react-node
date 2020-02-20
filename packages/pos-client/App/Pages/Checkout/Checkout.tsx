6fbc58ae25dafccf96be1796d5a607a5ccb8d2bf
Geoff Whitehead <geoff1012@gmail.com>
2020-02-20 14:41:30 +0000

    move populate inside authenticated section

--- packages/pos-client/App/Pages/Checkout/Checkout.tsx
+++ packages/pos-client/App/Pages/Checkout/Checkout.tsx
@@ -1,20 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Platform } from 'react-native'
 import { Container, Button, Text, Header, Drawer, Content } from '../../core'
-import { useRealmQuery } from '../../hooks/useRealm'
-import { realm } from '../../services/Realm'
 import { SidebarHeader } from '../../components/SidebarHeader/SidebarHeader'
-
+import { populate } from '../../services/populate'
+import { Loading } from '../Loading/Loading'
+import { useRealmQuery } from 'react-use-realm'
+import { ItemSchema } from '../../services/schemas'
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu.',
   android: 'Double tap R on your keyboard to reload,\nShake or press menu button for dev menu.',
 })
 
-const query = () => realm.objects('Item')
-
 export const Checkout = ({ navigation }) => {
-  const items = useRealmQuery(query)
-  return (
+  const [loading, setLoading] = useState(true)
+
+  React.useEffect(() => {
+    const populateAsync = async () => {
+      // TODO: use the dataId property to validate whether we need to re populate.
+      try {
+        await populate()
+        setLoading(false)
+      } catch (err) {
+        console.log('Populating failed', err)
+      }
+    }
+    populateAsync()
+  }, [])
+
+  const items = useRealmQuery({ source: ItemSchema.name })
+  console.log('items', items)
+  return loading ? (
+    <Loading />
+  ) : (
     <Container>
       <SidebarHeader title="Checkout" onOpen={navigation.toggleDrawer()} />
       <Content>
