c980f6c683d7528afd19d6389ee53a093762d6d1
Geoff Whitehead <geoff1012@gmail.com>
2020-02-22 13:36:36 +0000

    refactor linting

---
+++ packages/pos-client/.prettierrc.js
@@ -0,0 +1,23 @@
+// {
+//   printWidth : 100,
+//   tabWidth : 2,
+//   useTabs : false,
+//   semi : false,
+//   singleQuote : true,
+//   trailingComma : "es5",
+//   bracketSpacing : true,
+//   jsxBracketSameLine : false,
+//   arrowParens : "always",
+//   rangeStart : 0,
+//   parser : "babel",
+//   requirePragma : false,
+//   insertPragma : false,
+//   proseWrap : "preserve"
+// }
+module.exports = {
+  semi: true,
+  trailingComma: "all",
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 4
+};
