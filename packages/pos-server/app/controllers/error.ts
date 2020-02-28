268f2f1dafc7067ecc12b0092342ce0d84f139a0
Geoff Whitehead <geoff1012@gmail.com>
2020-02-28 21:36:10 +0000

    fixes compile and build errors

--- packages/pos-server/app/controllers/error.ts
+++ packages/pos-server/app/controllers/error.ts
@@ -1,29 +1,24 @@
 import { APP_NAME } from '../config';
-import { APIError } from '../helpers';
 
 const bodyParserHandler = (error, request, response, next) => {
     if (error instanceof SyntaxError || error instanceof TypeError) {
         // console.error(error);
-        return next(new APIError(400, 'Bad Request', 'Malformed JSON.'));
+        return next(new Error('400'));
     }
 };
 
 const notFoundHandler = (request, response, next) => {
-    return next(
-        new APIError(404, 'Resource Not Found', `${request.path} is not valid path to a ${APP_NAME} resource.`),
-    );
+    return next(new Error('404'));
 };
 
 const notAllowedHandler = (request, response, next) => {
-    return next(
-        new APIError(405, 'Method Not Allowed', `${request.method} method is not supported at ${request.path}.`),
-    );
+    return next(new Error('405'));
 };
 
 const serverErrorHandler = (error, request, response) => {
     let err = error;
-    if (!(error instanceof APIError)) {
-        err = new APIError(500, error.type, error.message);
+    if (!(error instanceof Error)) {
+        err = new Error('500');
     }
 
     return response.status(err.status).json(err);
--- packages/pos-server/app/helpers/index.ts
+++
@@ -1,3 +0,0 @@
-import APIError from './APIError';
-
-export { APIError };
