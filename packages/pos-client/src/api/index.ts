6c7ffbeea0116bd234a52cb15e8506ba464af51b
Geoff Whitehead <geoff1012@gmail.com>
2020-02-28 22:15:51 +0000

    config changes

--- packages/pos-client/src/api/index.ts
+++ packages/pos-client/src/api/index.ts
@@ -1,8 +1,13 @@
-import { create } from 'apisauce'
+import { create } from 'apisauce';
 
+// TODO: learn how to use env vars in react native ios builds
+const base = {
+  local: 'http://localhost:5000',
+  prod: 'https://positive-server.herokuapp.com',
+};
 const Api = create({
-  baseURL: 'http://localhost:5000',
+  baseURL: base.prod,
   headers: { Accept: 'application/json' },
-})
+});
 
-export { Api }
+export { Api };
