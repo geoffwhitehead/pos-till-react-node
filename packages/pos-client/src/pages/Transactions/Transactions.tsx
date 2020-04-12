5892b7a6fa45024c6b3132bd7d60441e4da38ac5
Geoff Whitehead <geoff1012@gmail.com>
2020-04-12 14:19:28 +0100

    refactor: move transactions list into own comp

--- packages/pos-client/src/pages/Transactions/Transactions.tsx
+++ packages/pos-client/src/pages/Transactions/Transactions.tsx
@@ -1,19 +1,16 @@
 import React, { useState, useContext } from 'react';
-import { Text, Container, Grid, Col, List, ListItem, Left, Body, Right, Content, Badge } from '../../core';
+import { Container, Grid, Col } from '../../core';
 import { SidebarHeader } from '../../components/SidebarHeader/SidebarHeader';
 import { Receipt } from '../Checkout/sub-components/Receipt/Receipt';
 import { useRealmQuery } from 'react-use-realm';
 import { BillProps, BillSchema } from '../../services/schemas';
-import { Collection } from 'realm';
-import dayjs from 'dayjs';
-import { total, formatNumber } from '../../utils';
-import { StyleSheet } from 'react-native';
 import { BillPeriodContext } from '../../contexts/BillPeriodContext';
+import { TransactionsList } from './sub-components/TransactionsList/TransactionsList';
 
-const symbol = '£'; // TODO move
 interface TransactionsProps {
   navigation: any; // TODO
 }
+
 export const Transactions: React.FC<TransactionsProps> = ({ navigation }) => {
   const { billPeriod, setBillPeriod } = useContext(BillPeriodContext);
   const closedBills = useRealmQuery<BillProps>({
@@ -38,62 +35,3 @@ export const Transactions: React.FC<TransactionsProps> = ({ navigation }) => {
     </Container>
   );
 };
-
-interface TransactionsListProps {
-  bills: Collection<BillProps>;
-  onSelectBill: (bill: BillProps) => void;
-  selectedBill?: BillProps;
-}
-
-const TransactionsList: React.FC<TransactionsListProps> = ({ bills, onSelectBill, selectedBill }) => {
-  const selectBillFactory = (index: number) => () => onSelectBill(bills[index]);
-  return (
-    <Content>
-      <List>
-        <ListItem itemHeader>
-          <Left>
-            <Text>Bill / Time</Text>
-          </Left>
-          <Body>
-            <Text>Total</Text>
-          </Body>
-          <Right>
-            <Text>Payment Methods</Text>
-          </Right>
-        </ListItem>
-        {bills.map((bill, index) => {
-          const isSelected = selectedBill && bill._id === selectedBill._id;
-          return (
-            <ListItem noIndent style={isSelected && styles.selected} key={bill._id} onPress={selectBillFactory(index)}>
-              <Left>
-                <Badge style={{ minWidth: 28 }} success>
-                  <Text>{bill.tab}</Text>
-                </Badge>
-                <Text>{` / ${dayjs(bill.closedAt)
-                  .format('HH:mm')
-                  .toString()}`}</Text>
-              </Left>
-              <Body>
-                <Text>{formatNumber(total(bill), symbol)}</Text>
-              </Body>
-              <Right>
-                <Text>
-                  {bill.payments
-                    .reduce(
-                      (acc, payment) => (acc.includes(payment.paymentType) ? acc : [...acc, payment.paymentType]),
-                      [],
-                    )
-                    .join(', ')}
-                </Text>
-              </Right>
-            </ListItem>
-          );
-        })}
-      </List>
-    </Content>
-  );
-};
-
-const styles = StyleSheet.create({
-  selected: { backgroundColor: '#cde1f9' },
-});
