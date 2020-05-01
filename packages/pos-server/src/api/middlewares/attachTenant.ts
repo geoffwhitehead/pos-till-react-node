f29994af08eb6043dafaadbea5314d58962d6119
Geoff Whitehead <geoff1012@gmail.com>
2020-05-01 20:12:41 +0100

    fix: check for organization when attaching tenant

--- packages/pos-server/src/api/middlewares/attachTenant.ts
+++ packages/pos-server/src/api/middlewares/attachTenant.ts
@@ -1,5 +1,7 @@
 import { Container } from 'typedi';
 import { Logger } from 'winston';
+import { OrganizationService } from '../../services/organization';
+import { objectId } from '../../utils/objectId';
 /**
  * Set tenant on Container
  * @param {*} req Express req Object
@@ -8,12 +10,22 @@ import { Logger } from 'winston';
  */
 const attachTenant = async (req, res, next) => {
     const logger = Container.get('logger') as Logger;
-    console.log('req.token', req.token);
+    const organizationService = Container.get('organizationService') as OrganizationService;
+
+    logger.debug('req.token', req.token);
 
     if (!req.token) {
         return next();
     }
     try {
+        const organization = await organizationService.findById(objectId(req.token.organizationId));
+        console.log('org', organization);
+        if (!organization) {
+            res.status(400).json({
+                error: { message: 'Organization not found when attaching tenant', code: 'NOT_FOUND' },
+            });
+            return;
+        }
         Container.set('organizationId', req.token.organizationId);
         return next();
     } catch (e) {
