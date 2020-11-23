9ee7ad44fc52e1e67566a0c3076feca65bd55447
Geoff Whitehead <geoff1012@gmail.com>
2020-11-23 11:45:26 +0000

    add sync retry

--- packages/pos-client/src/services/sync.ts
+++ packages/pos-client/src/services/sync.ts
@@ -1,25 +1,36 @@
 import { Database } from '@nozbe/watermelondb';
 import { synchronize } from '@nozbe/watermelondb/sync';
+import retry from 'async-retry';
 import { pullChanges, pushChanges } from '../api/sync';
 
 export const sync = async (database: Database) => {
   await synchronize({
     database,
     pullChanges: async ({ lastPulledAt }) => {
-      const response = await pullChanges({ lastPulledAt, schemaVersion: '21', migration: null });
+      return await retry(
+        async bail => {
+          const response = await pullChanges({ lastPulledAt, schemaVersion: '21', migration: null });
 
-      if (!response.ok || !response.data.changes) {
-        throw new Error('Sync pull failed');
-      }
+          if (!response.ok || !response.data.changes) {
+            bail(new Error('Sync pull failed'));
+          }
 
-      const { changes, timestamp } = response.data;
-      return { changes, timestamp };
+          const { changes, timestamp } = response.data;
+          return { changes, timestamp };
+        },
+        { retries: 2 },
+      );
     },
     pushChanges: async ({ changes, lastPulledAt }) => {
-      const response = await pushChanges({ lastPulledAt, changes: JSON.stringify(changes) });
-      if (!response.ok) {
-        throw new Error('Sync push failed');
-      }
+      return await retry(
+        async bail => {
+          const response = await pushChanges({ lastPulledAt, changes: JSON.stringify(changes) });
+          if (!response.ok) {
+            bail(new Error('Sync push failed'));
+          }
+        },
+        { retries: 2 },
+      );
     },
     // migrationsEnabledAtVersion: 1,
   });
