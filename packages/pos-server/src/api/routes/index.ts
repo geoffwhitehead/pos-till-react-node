09cf79748f8b6a82589aa4ab10650accc1cf1576
Geoff Whitehead <geoff1012@gmail.com>
2021-01-29 10:14:48 +0000

    remove unused api files pending refactor

--- packages/pos-server/src/api/routes/index.ts
+++ packages/pos-server/src/api/routes/index.ts
@@ -1,26 +1,14 @@
 import { Router } from 'express';
-import userRoutes from './user';
 import authRoutes from './auth';
-import productRoutes from './product';
 import maintenanceRoutes from './maintenance';
-import printerRoutes from './printer';
 import organizationRoutes from './organization';
-import printerGroupRoutes from './printerGroup';
 import syncRoutes from './sync';
+import userRoutes from './user';
 
 export default () => {
     const app = Router();
 
-    [
-        userRoutes,
-        authRoutes,
-        productRoutes,
-        maintenanceRoutes,
-        printerRoutes,
-        organizationRoutes,
-        printerGroupRoutes,
-        syncRoutes,
-    ].map(route => route(app));
+    [userRoutes, authRoutes, maintenanceRoutes, organizationRoutes, syncRoutes].map(route => route(app));
 
     return app;
 };
